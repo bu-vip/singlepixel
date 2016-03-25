6c9c28d70d92e591ad3b9b058038852af8d3ea34
Doug Roeper <douglas.roeper@gmail.com>
2016-03-25 09:52:45 -0400

    -Removed unnecessary R files

--- r/plotSensorData.R
+++
@@ -1,5 +0,0 @@
-#sensorName <- readline("Enter the sensor data filename: ")
-sensorName <- "take1_raw.txt"
-sensorData <- fromJSON(readChar(sensorName, file.info(sensorName)$size))
-sensorData$luminance <- 0.2989 * sensorData$red + 0.5870 * sensorData$green + 0.1140 * sensorData$blue
-ggplot(sensorData, aes(received, luminance)) + geom_line() + facet_grid(sensorId ~ .)
