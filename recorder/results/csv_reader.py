314816816f2532598ba4686479b4c935ee3ffce1
Eddy Luo <edluo1@bu.edu>
2016-07-07 11:13:20 -0400

    Updated files to work with a lot of gesture data

--- recorder/results/csv_reader.py
+++ recorder/results/csv_reader.py
@@ -1,5 +1,6 @@
 # Import CSV to read
 import csv
+import os
 
 import matplotlib.patches as mpatches
 import matplotlib.pyplot as plt
@@ -8,7 +9,8 @@ import numpy as np
 
 # Output is a list of values for cameras for blue, green, and red
 # from the recorder.
-def readCSV(fileName, numOfCameras = 6):
+
+def readCSV(fileName, numOfCameras = 12):
     N = 0
     blue, red, green = ([[] for j in range(numOfCameras)] for i in range(3))
     try:
@@ -62,6 +64,33 @@ def readCSV_white(fileName, numOfCameras = 12):
         print(e)
     return []
 
+def readCSV_convert(fileName, numOfCameras = 12):
+    N = 0
+    white = [[] for j in range(numOfCameras)]
+    try:
+        f = open(fileName, "r")
+        with f:
+            reader = csv.reader(f)
+            normalized = False
+            for row in reader:
+                if N != 0:
+                    cameraId = 8*int(row[2])+int(row[5])
+                    if (normalized or cameraId == 0) and cameraId < numOfCameras: # ignore results before normalization
+                        normalized = True # normalize it
+                        redValue = float(row[4])
+                        greenValue = float(row[1])
+                        blueValue = float(row[0])
+                        whiteValue = 0.299*redValue + 0.587*greenValue + 0.114*blueValue
+                        white[cameraId].append(whiteValue)
+                N = N+1
+            for i in range(len(white)):
+                if len(white[numOfCameras-1]) < len(white[i]):
+                    white[i].pop()
+            return white
+
+    except Exception as e:
+        print(e)
+    return []
 
 
 def graph_plots(plots):
@@ -73,6 +102,18 @@ def graph_plots(plots):
         plt.scatter(range(len(plots[i])), plots[i], color=inputColor)
     plt.legend(handles=patches)
     plt.show()
-# SIF BU
 
+def collectData(mainDirectory = os.getcwd()):
+    allData = dict() # store everything in a dictionary
+    for subdir, dirs, files in os.walk(mainDirectory):
+        gestureName = os.path.basename(subdir)
+        if gestureName != "results":
+            if not gestureName.startswith("_"):
+                gestureList = list()
+                for fileName in os.listdir(gestureName):
+                    print(fileName)
+                    gestureList.append(readCSV_convert(gestureName+"/"+fileName))
+            allData[gestureName] = gestureList
+    return allData
+    
 
--- recorder/results/gesture_identify.py
+++ recorder/results/gesture_identify.py
@@ -1,16 +1,18 @@
-from csv_reader import readCSV_white, graph_plots
+from csv_reader import readCSV_white, readCSV_convert, graph_plots
 from scipy import interpolate
 import numpy as np
 
 numOfCameras = 12
+thrsh = 1e-10
 
-stand_results = readCSV_white("results-stand.txt", numOfCameras)
-spin_results = readCSV_white("results-spin.txt", numOfCameras)
-write_results = readCSV_white("results-write.txt", numOfCameras)
+stand_results = readCSV_convert("results-stand0.txt", numOfCameras)
+stand_rgb = readCSV_convert("results-stand0.txt", numOfCameras)
+spin_results = readCSV_convert("results-spin.txt", numOfCameras)
+write_results = readCSV_convert("results-write.txt", numOfCameras)
 
-no_results = readCSV_white("results-nothing.txt", numOfCameras)
+no_results = readCSV_convert("results-nothing.txt", numOfCameras)
 
-identifyTest = readCSV_white("test-result.txt", numOfCameras)
+identifyTest = readCSV_convert("test-result.txt", numOfCameras)
 identification_dict = {
     'standing': stand_results,
     'spinning': spin_results,
@@ -18,32 +20,40 @@ identification_dict = {
     'nothing': no_results
     }
 
+def cutData(funct): # Input a function and the derivative will be found where
+    # the data is to be cut off.
+    
+    
+
 def identify_gesture(test, trainingData=identification_dict):
     if isinstance(test, str):
-        test_read = readCSV_white(test, numOfCameras)
-        test_result = interpolate_result(test_read)
+        test_read = readCSV_convert(test, numOfCameras)
+        test_result = interpolate_result(normalize(test_read))
     else:
-        test_result = interpolate_result(test)
+        test_result = interpolate_result(normalize(test))
     interp_data = dict() # store interpreted data in a dictionary
     data_sum = dict()
     for name, dataset in trainingData.items(): # dataset
-        interp_data[name] = interpolate_result(dataset)
+        interp_data[name] = interpolate_result(normalize(dataset))
         data_sum[name] = 0.0
     
     for name, func in interp_data.items():
         for n in np.linspace(0., 1., 200):
-            func_values = np.array(func(n))
-            test_values = np.array(test_result(n))
-            diff = (func_values - test_values)
-            diff_sum = np.sum(np.square(diff))
+            func_values = np.array(func(n)) # result of training data function
+            test_values = np.array(test_result(n)) # result of test function
+            diff = (func_values - test_values) # get difference
+            diff_sum = np.sum(np.square(diff)) # get sum
             data_sum[name] += diff_sum
     print(data_sum) # test function to show the differences
     return min(data_sum, key=data_sum.get)
                 
-def listAverage(inputList):
+def normalize(inputList):
     averages = []
     for i in range(len(inputList)):
-        averages.append(np.mean(inputList[i]))
+        if (np.std(inputList[i]) > thrsh): # floating point error handling/close to 0
+            averages.append((inputList[i]-np.mean(inputList[i]))/np.std(inputList[i]))
+        else:
+            averages.append(inputList[i]-np.mean(inputList[i])) 
     return averages
     
 def interpolate_result(result_list): # Returns interpolation function normalized from 0 to 1
